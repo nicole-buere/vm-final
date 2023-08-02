85b82d863665b37197f176d28bcf2f72ae1d41e8
nicole-buere <140893902+nicole-buere@users.noreply.github.com>
2023-08-02 17:12:38 +0800

    Update RegularItemsTableModel.java

--- RegularItemsTableModel.java
+++ RegularItemsTableModel.java
@@ -1,4 +1,8 @@
-
+/**
+ * This class represents a custom Table Model for regular items in a menu.
+ * It extends the DefaultTableModel to provide additional functionality
+ * for managing regular items data.
+ */
 package Main;
 
 import java.util.ArrayList;
@@ -8,7 +12,12 @@ import javax.swing.table.AbstractTableModel;
 
 
 public class RegularItemsTableModel extends DefaultTableModel {
-
+/**
+*<p>
+* Constructs a RegularItemsTableModel object with predefined columns and rows.
+* Each row represents a regular item with its properties: Slot, Name, Calories,
+* Quantity, and Price.
+*/
     public RegularItemsTableModel() {
         addColumn("Slot");
         addColumn("Name");
@@ -26,7 +35,10 @@ public class RegularItemsTableModel extends DefaultTableModel {
         addRow(new Object[]{8, "truffle nachos", 352, 10, 310});
     
     }
-    
+    /**
+     * Retrieves the data from the table model and returns it as a List of Object arrays.
+     * @return A List containing Object arrays, where each array represents a row of data.
+     */
      public List<Object[]> getData() {
         List<Object[]> data = new ArrayList<>();
         for (int row = 0; row < getRowCount(); row++) {
@@ -38,11 +50,20 @@ public class RegularItemsTableModel extends DefaultTableModel {
         }
         return data;
     }
-    
+    /**
+     * Notifies all listeners that the table data has changed.
+     * This method is used to refresh the table when the underlying data is modified.
+     */
       public void fireTableDataChanged() {
         fireTableChanged(null);
     }
-
+/**
+* Specifies whether a cell is editable or not.
+* In this implementation, all cells are set as non-editable.
+* @param row The row index of the cell.
+* @param column The column index of the cell.
+* @return false to make the cell non-editable, true otherwise.
+*/
     @Override
     public boolean isCellEditable(int row, int column) {
         return false;
